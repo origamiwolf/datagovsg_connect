7f9ed0a68c64f29777aea0fd8dbc181f0022c0ea
origamiwolf <origamiwolf@gmail.com>
2019-05-16 22:07:51 +0800

    Added primitive datetime parsing

--- Code.js
+++ Code.js
@@ -10,11 +10,13 @@ function getAuthType() {
 function getConfig(request) {
   var cc = DataStudioApp.createCommunityConnector();
   var config = cc.getConfig();
+
+  // todo: validation
   
-  config.newInfo()  
+  config.newInfo()
   .setId('Instructions')
   .setText('Enter dataset name to connect to.');
-         
+
   config.newTextInput()
   .setId('dataset')
   .setName('Enter the name of a dataset')
@@ -28,18 +30,16 @@ function getFields(request) {
   var cc = DataStudioApp.createCommunityConnector();
   var myfields = cc.getFields();
   var types = cc.FieldType;
-  
+
   var url = [
     'https://data.gov.sg/api/action/package_show?id=',
     request.configParams.dataset
   ];
   var response = UrlFetchApp.fetch(url.join(''));
-  var parsedResponse = JSON.parse(response).result.resources[0].fields;  
+  var parsedResponse = JSON.parse(response).result.resources[0].fields;
   var datasetID = JSON.parse(response).result.resources[0].id;
-  var myfieldtype; 
-
-// this needs to be more intelligent.  So far only numbers can be identified, everything else needs to be manually typed
-  
+  var myfieldtype;
+  // TODO: add more datetime formats
   parsedResponse.forEach(function(x) {
     switch(x["type"]) {
       case "numeric":
@@ -47,20 +47,37 @@ function getFields(request) {
         .setId(x["name"])
         .setType(types.NUMBER)
         break;
+      case "datetime":
+        switch(x["format"]) {
+          case "YYYY-MM-DD":
+            datetimetype = types.YEAR_MONTH_DAY;
+            break;           
+          case "YYYY-MM":
+            datetimetype = types.YEAR_MONTH;
+            break;
+          case "YYYY":
+            datetimetype = types.YEAR;
+            break;            
+          default:
+            datetimetype = types.TEXT;
+            break;
+        }
+        myfields.newDimension()
+          .setId(x["name"])
+          .setType(datetimetype)
+        break;
       default:
         myfields.newDimension()
         .setId(x["name"])
         .setType(types.TEXT)
-        break;    
-    }  
+        break;
+    }
   });
-  
   request.configParams.dataset = datasetID;
-  
   return myfields;
 }
 
-function getSchema(request) { 
+function getSchema(request) {
   var fields = getFields(request).build();
   return { schema: fields, config: "dataset" }
 }
@@ -69,31 +86,40 @@ function responseToRows(requestedFields, response) {
   return response.map(function(getRowData) {
     var row = [];
     requestedFields.asArray().forEach(function(field) {
-      Logger.log(field.getId());      
-      row.push(getRowData[field.getId()]);
-      Logger.log(row);
+      var cc = DataStudioApp.createCommunityConnector();
+      var types = cc.FieldType;      
+      // Need to ensure data formats for datetime fits!
+      field_value = getRowData[field.getId()];
+      switch (field.getType()) {
+        case types.YEAR_MONTH_DAY:
+        case types.YEAR_MONTH:
+          field_value = field_value.replace(/-/g,'');
+          break;
+      }    
+      row.push(field_value);
   });
     return { values: row };
   });
 }
 
 function getData(request) {
+  // todo query size
   var requestedFieldIds = request.fields.map(function(field) {
     return field.name;
   });
   var requestedFields = getFields(request).forIds(requestedFieldIds);
-  
+
   var url = [
     'https://data.gov.sg/api/action/datastore_search?resource_id=',
-    request.configParams.dataset
+    request.configParams.dataset,
+    '&limit=10'
   ];
   var response = UrlFetchApp.fetch(url.join(''));
   var parsedResponse = JSON.parse(response).result.records;
   var rows = responseToRows(requestedFields, parsedResponse);
-  
+
   return {
     schema: requestedFields.build(),
     rows: rows
   };
 }
-
